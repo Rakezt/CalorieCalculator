ea44e73b0d17e6aaa6e2914597a8ebf905c099db
L Rakesh Singh <keztmckinnon@gmail.com>
2022-10-08 15:32:06 +0530

    init

---
+++ script.js
@@ -0,0 +1,29 @@
+var female=document.querySelector("#female")
+var male=document.querySelector("#male")
+var weight=document.querySelector("#weight")
+var heightInFeet=document.querySelector("#height-In-Feet")
+var heightInInches=document.querySelector("#height-In-Inches")
+var age=document.querySelector("#age")
+var lifeStyle=document.querySelector("#lifeStyle")
+var calculateBtn=document.querySelector("#cal-btn")
+var output=document.querySelector("#output")
+
+alert("We wil never stored your data")
+
+
+calculateBtn.addEventListener("click", calorieCal)
+
+function calorieCal(){
+    if(female.checked){
+        output.innerText=  `You need ${Math.round(Number(lifeStyle.value)*655 + ( 9.563*Number(weight.value)) + ( 1.850 * ((Number(heightInFeet.value)*12)+Number(heightInInches.value)))-(4.676*Number(age.value)))} calorie per day`
+        
+    } else if (male.checked){
+        output.innerText=`You need ${Math.round(Number(lifeStyle.value)* 66.5 + ( 13.76 * Number(weight.value) ) + ( 5.003 * ((Number(heightInFeet.value)*12)+Number(heightInInches.value))) - ( 6.755 * Number(age.value) ))} calorie per day`
+    } else {
+        output.innerText= `Something's wrong, please check`
+    }
+}
+
+
+//female BMR = 66 + ( 6.2 × weight in pounds ) + ( 12.7 × height in inches ) – ( 6.76 × age in years )
+//Male	BMR = BMR = 66.5 + ( 13.76 × weight in kg ) + ( 5.003 × height in cm ) – ( 6.755 × age in years )
